be78a7cf390f186d439dd225157cb38294debade
Vojtěch Tomas <tomas@vojtatom.cz>
2021-08-04 17:03:48 +0200

    WIP Tiles

--- scripts/tilessegment.py
+++ scripts/tilessegment.py
@@ -1,6 +1,5 @@
 from argparse import ArgumentParser
 
-from numpy.lib.shape_base import tile
 from metacity.geometry.bbox import vertices_bbox
 from metacity.project import MetacityLayer, MetacityProject
 
@@ -23,10 +22,86 @@ def process_args():
 class MetaTile:
     def __init__(self, bbox):
         self.bbox = bbox
+
         
     def __str__(self):
         return f"{self.bbox}"
 
+
+
+def special_case_two_on_plane(z, plane, triangle):
+    if z > plane:
+        return [], [ triangle ]
+    else: 
+        return [ triangle ], []
+
+
+def special_case_one_on_plane_split(y, z, plane, triangle, on_plane_idx, y_idx, z_idx):
+    p = (plane - z) / (y - z)
+    mid = triangle[y_idx] * p + triangle[z_idx] * (1 - p)
+    y_triangle = np.array([triangle[y_idx], mid, triangle[on_plane_idx]])
+    z_triangle = np.array([triangle[on_plane_idx], mid, triangle[z_idx]])
+    if y < plane and z > plane:
+        return y_triangle, z_triangle
+    else:
+        return z_triangle, y_triangle
+
+
+def special_case_one_on_plane(y, z, plane, triangle, on_plane_idx, y_idx, z_idx):
+    if y > plane and z > plane:
+        return [], [ triangle ]
+    elif y < plane and z < plane:
+        return [ triangle ], []
+    else:
+        return special_case_one_on_plane_split(y, z, plane, triangle, on_plane_idx, y_idx, z_idx)
+
+
+def handle_special_cases(triangle, plane, axis):
+    a, b, c = triangle
+    a_onplane, b_onplane, c_onplane = (triangle[:, 0] == plane)
+
+    if a_onplane and b_onplane and c_onplane:
+        return [ triangle ], []
+
+    if a_onplane and b_onplane:
+        return special_case_two_on_plane(c[axis], plane, triangle)
+    
+    if b_onplane and c_onplane:
+        return special_case_two_on_plane(a[axis], plane, triangle)
+    
+    if c_onplane and a_onplane:
+        return special_case_two_on_plane(b[axis], plane, triangle)
+
+    if a_onplane:
+        return special_case_one_on_plane(b[axis], c[axis], plane, triangle, 0, 1, 2)
+
+    if b_onplane:
+        return special_case_one_on_plane(c[axis], a[axis], plane, triangle, 1, 2, 0)
+
+    if c_onplane:
+        return special_case_one_on_plane(a[axis], b[axis], plane, triangle, 2, 0, 1)
+
+
+def x_split_triangle(triangle, plane):
+    onplane = (triangle[:, 0] == plane)
+
+    if np.any(onplane):
+        return handle_special_cases(triangle, plane, 0)
+
+    #TODO
+
+
+def x_split(triangles, planes):
+    planes.sort()
+    segmented = []
+
+    for plane in planes:
+        for triangle in triangles:
+            x_split_triangle(triangle, plane)
+
+
+
+    #TODO
 
 
 class MetaRegularGrid:
@@ -34,6 +109,7 @@ class MetaRegularGrid:
         self.bbox = bbox
         self.shift = self.bbox[0]
         self.tile_size = tile_size
+        #self.epsilon = tile_size * 0.0001 # test this value TBA
         self.tiles = {}
         self.__init_grid(bbox, tile_size)
 
@@ -44,31 +120,69 @@ class MetaRegularGrid:
         for x in range(resolution[0]):
             self.tiles[x] = {}
             for y in range(resolution[1]):
-                self.__init_tile(bbox, tile_size, x, y)
+                self.__init_tile(bbox, x, y)
 
 
-    def __init_tile(self, bbox, tile_size, x, y):
-        tile_base = [ self.shift[0] + x * tile_size, self.shift[1] + y * tile_size, bbox[0, 2] ]
-        tile_top = [ self.shift[0] + (x + 1) * tile_size, self.shift[1] + (y + 1) * tile_size, bbox[1, 2] ]
+    def __init_tile(self, bbox, x, y):
+        tile_base = [ self.__x_tile_base(x), self.__y_tile_base(y), bbox[0, 2] ]
+        tile_top = [ self.__x_tile_top(x), self.__y_tile_top(y), bbox[1, 2] ]
         tile_bbox = np.array([tile_base, tile_top])
         self.tiles[x][y] = MetaTile(tile_bbox)
 
 
+    def __x_tile_base(self, x):
+        return self.shift[0] + x * self.tile_size
+
+
+    def __y_tile_base(self, y):
+        return self.shift[1] + y * self.tile_size
+
+
+    def __x_tile_top(self, x):
+        return self.shift[0] + (x + 1) * self.tile_size
+
+
+    def __y_tile_top(self, y):
+        return self.shift[1] + (y + 1) * self.tile_size
+
+
+
+
     def triangle_tile_index(self, triangle):
         return np.floor(((triangle - self.shift) / self.tile_size)[:, 0:2]).astype(int)
+
+
+    def split_planes(self, indices):
+        min_box = np.amin(indices, axis=0)
+        max_box = np.amax(indices, axis=0)
+
+        x_planes = []
+        for x in range(min_box[0], max_box[0]):
+            x_planes.append(self.__x_tile_top(x))
+
+        y_planes = []
+        for y in range(min_box[1], max_box[1]):
+            y_planes.append(self.__y_tile_top(y))
+
+        return x_planes, y_planes
+                
                 
 
     def insert(self, triangle):
         indices = self.triangle_tile_index(triangle)
-        
+
         if np.array_equal(indices[0], indices[1]) and np.array_equal(indices[0], indices[2]):
             pass
-            #all equal
+            #all equal, not neccasary to slice
         else:
-            print(triangle)
             print(indices)
-            quit()
+            x_planes, y_planes = self.split_planes(indices)
+            print(x_planes, y_planes)
 
+            triangles = [ triangle ]
+            print(triangles)
+            triangles = x_split(triangles, x_planes)
+            quit()
 
 
 
