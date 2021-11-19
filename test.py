f6307b9673282ee33d163133cbcbac17ce58dc7c
shanataru <shanataru@gmail.com>
2021-11-19 15:57:58 +0100

    test parser

---
+++ test.py
@@ -0,0 +1,13 @@
+import json
+from metacity.io.sim import parser
+#from metacity.io.shapefile import parser
+
+def main():
+    input_file = "/home/metakocour/Projects/Metacity/metacity/io/sim/car_sec_0.json"
+    #input_file2 = '/home/metakocour/Projects/Metacity/metacity/io/sim/OBYVATELSTVO/'
+
+    asd = parser.parse(input_file)
+    print(asd)
+
+if __name__ == "__main__":
+    main()
