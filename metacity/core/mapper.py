31b2c6c2802e28d00d1187cb8c28a3f195a08cc4
Vojtěch Tomas <tomas@vojtatom.cz>
2021-12-09 18:35:35 +0100

    Added BBox mapping

--- metacity/core/mapper.py
+++ metacity/core/mapper.py
@@ -1,8 +1,9 @@
+from typing import List
 from metacity.datamodel.layer import Layer, LayerOverlay
 from metacity.core.grid.grid import Grid
 from metacity.core.timeline import Timeline
-from metacity.geometry import MultiTimePointMapper, MultiTimePoint
-
+from metacity.geometry import MeshOIDMapper, MultiTimePointMapper, MultiTimePoint
+from tqdm import tqdm
 
 def build_overlay(overlay: LayerOverlay, source: Layer, target: Layer, progressCallback=None):
     if source.type != "layer" or target.type != "layer":
@@ -61,6 +62,43 @@ def build_overlay_grid(overlay: LayerOverlay, source: Layer, target: Layer, prog
 
 
 
-def centroid_mapping(source: Layer, target: Layer, progressCallback=None):
+def bbox_attribute_mapping(source: Layer, target: Layer, attributes: List[str]):
     if source.type != "layer" or target.type != "layer":
         raise Exception(f"Cannot map type {source.type} to {target.type}, only layer to layer is supported")
+
+    for o in tqdm(target.objects):
+        for attribute in attributes:
+            if attribute in o.meta:
+                del o.meta[attribute]
+
+    grid = Grid(target)
+    tiles = [tile.polygon for tile in tqdm(grid.tiles) if tile.polygon is not None]
+
+    mapper = MeshOIDMapper(tiles)
+
+    grid = Grid(source)
+    for tile in tqdm(grid.tiles):
+        p = tile.polygon
+        if p is None:
+            continue
+        
+        mapper.map_oids(p)
+
+    #target oid -> source oid
+    mapping = mapper.mapping
+    raw_mapping = mapper.raw_mapping
+    with open("oid_map.txt", "w") as f:
+        for oid, source_oid in tqdm(raw_mapping.items()):
+            f.write("{} {}\n".format(oid, source_oid))
+
+    target_objects = [ key for key in mapping.keys() ]
+    target_objects.sort()
+
+    for toid in tqdm(target_objects):
+        source_oid = mapping[toid]
+        m = source[source_oid].meta
+        for attribute in attributes:
+            if attribute in m:
+                target[toid].meta[attribute] = m[attribute]
+    
+    target.persist()
--- todos/oid_map.py
+++ todos/oid_map.py
@@ -1,44 +1,34 @@
 from metacity.datamodel.project import Project
-from metacity.core.grid.grid import Grid
-from metacity.geometry import MeshOIDMapper
+from metacity.core.mapper import bbox_attribute_mapping
 from tqdm import tqdm
 
 p = Project("Praha")
 source = p.get_layer("Obyvatelstvo")
 target = p.get_layer("Budovy")
-attribute = "Sum_PTOTAL"
+count = "Sum_PTOTAL"
+area = "Shape_Area"
+attributes = [count, area]
+bbox_attribute_mapping(source, target, attributes)
 
+maximum = 0
+avg = 0
+cnt = 0
 
 for o in tqdm(target.objects):
-    if attribute in o.meta:
-        del o.meta[attribute]
+    if count in o.meta and area in o.meta:
+        o.meta[count] = o.meta[count] / o.meta[area]
+        avg += o.meta[count]
+        cnt += 1
+        maximum = max(maximum, o.meta[count])
 
-grid = Grid(target)
-tiles = [tile.polygon for tile in tqdm(grid.tiles) if tile.polygon is not None]
+print(maximum)
+maximum /= 1000
 
-mapper = MeshOIDMapper(tiles)
+for o in tqdm(target.objects):
+    if count in o.meta and area in o.meta:
+        o.meta[count] /= maximum
 
-grid = Grid(source)
-for tile in tqdm(grid.tiles):
-    p = tile.polygon
-    if p is None:
-        continue
-    
-    mapper.map_oids(p)
+avg = avg / cnt
+print(avg * 1000)
 
-#target oid -> source oid
-mapping = mapper.mapping
-raw_mapping = mapper.raw_mapping
-with open("oid_map.txt", "w") as f:
-    for oid, source_oid in tqdm(raw_mapping.items()):
-        f.write("{} {}\n".format(oid, source_oid))
-
-target_objects = [ key for key in mapping.keys() ]
-target_objects.sort()
-
-for toid in tqdm(target_objects):
-    source_oid = mapping[toid]
-    m = source[source_oid].meta
-    if attribute in m:
-        target[toid].meta[attribute] = m[attribute]
 target.persist()
