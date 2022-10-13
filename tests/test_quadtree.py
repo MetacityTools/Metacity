02fe99d163e4fac42d6c42a9c16956aa1093df9c
Vojtěch Tomas <tomas@vojtatom.cz>
2022-10-13 14:55:05 +0200

    Repair tests

--- tests/test_quadtree.py
+++ tests/test_quadtree.py
@@ -6,9 +6,12 @@ def test_build_quadtree(geometry_directory: str, tmp_directory: str):
     objects = [ o for o in parse_recursively(geometry_directory) ]
     assert len(objects) == 2826 + 5041 + 24
 
-    max_depth = 10
+    max_depth = 5
     tree = QuadTree(objects, max_depth)
-    tree.to_json(tmp_directory)
+    tree.to_json(tmp_directory, 3)
+
+    import os
+    assert os.path.exists(os.path.join(tmp_directory, "meta.json"))
     
 
 
